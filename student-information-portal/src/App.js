183e63f77069294b69f7155b56a057792e84c8a5
Nazik <151235734+MohamadNazik@users.noreply.github.com>
2024-11-19 08:45:47 +0000

    header created

--- student-information-portal/src/App.js
+++ student-information-portal/src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="outerDiv">
+    <div>
+      <h1 id='heading'>Student Information Portal</h1>
+      <hr/>
+      <div className='buttons-container'>
+        <p>Font Size:</p>
+        <button>S</button>
+        <button>M</button>
+        <button>L</button>
+      </div>
+      <hr/>
+      
       
     </div>
   );
---
+++ student-information-portal/src/components/studentTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function studentTable(){
+    return <div>
+        
+    </div>
+}
+
+export default studentTable;
